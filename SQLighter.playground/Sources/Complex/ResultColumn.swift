3ff9b0df52cc96193ae144ad350c8d2cff045e72
DongXu <cikelengfeng@gmail.com>
2016-09-16 00:47:40 +0800

    [DEL] playground

--- SQLighter.playground/Sources/Complex/ResultColumn.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public class ResultColumn: SQLRule {
-    public func allColumn() -> Self {
-        let keyword = SQLKeyword(".*")
-        return self.append(keyword)
-    }
-}
