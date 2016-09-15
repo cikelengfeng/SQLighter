57043792123da5e437e55631e5cd1217ba6825f9
DongXu <cikelengfeng@gmail.com>
2016-09-15 14:13:56 +0800

    [FIX] compile error

--- SQLighter/SQLighter/main.swift
+++ SQLighter/SQLighter/main.swift
@@ -13,16 +13,32 @@ let select = SimpleSelect()
 select
     .select(["c1","c3"])
     .from("tbl_tags")
-    .wherec(id("a") == "x",
+    .where_(id("a") == "x",
             id("b") < 1,
             id("c") != "ono",
-            id("d").inSet(["1","b","c","123"]))
+            id("d").in_(["1","b","c","123"]))
     .orderBy([("c1", Order.ASC), ("c2", Order.DESC)])
     .offset(42)
     .limit(22)
 
 print(select.assemble())
 print(select.parameters())
+
+let select2 = SimpleSelect()
+
+select2
+    .select(["c1","c3"])
+    .from("tbl_tags")
+    .where_(id("a") == "x",
+        id("b") < 1,
+        id("c") != "ono",
+        id("d").in_(["1","b","c","123"]))
+    .orderBy([("c1", Order.ASC), ("c2", Order.DESC)])
+    .offset(42)
+    .limit(22)
+
+let un = union(select, lhs: select2)
+print(un.assemble())
 
 let insert = SimpleInsert()
 
@@ -40,24 +56,27 @@ update
     .update()
     .table("tbl_people")
     .set(["c1":"x1","c2":"x2"])
-    .wherec(id("a") == "x",
-            id("b") < "y",
+    .where_(id("a") == "x",
+            (id("b") < "y"),
+            or(),
             id("c") <> "ono",
-            id("d").inSet(["1","b","c","123"]))
+            id("d").not().in_(["1","b","c","123"]))
 print(update.assemble())
 
 
 let delete = SimpleDelete()
 delete
     .delete("tbl_medias")
-    .wherec(
-            [
-            id("a") == "x",
-            id("b") < "y"],
+    .where_(
+            enclosed(
+                id("a") == "x",
+                and(),
+                id("b") < "y"),
             or(),
-            [
-            id("c") <> "ono",
-            id("d").inSet(["1","b","c","123"])]
+            enclosed(
+                id("c") <> "ono",
+                and(),
+                id("d").in_(["1","b","c","123"]))
     )
 
 print(delete.assemble())
