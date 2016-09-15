103ccb6c1dc02b70e32af125f412bf7ba4e817c2
DongXu <cikelengfeng@gmail.com>
2016-09-15 14:12:52 +0800

    [ADD] union

--- SQLighter/SQLighter/Simple/Helper.swift
+++ SQLighter/SQLighter/Simple/Helper.swift
@@ -47,3 +47,11 @@ public func or() -> SQLStmt {
     let pureSQL = SQLStmt("OR", params: [])
     return pureSQL
 }
+
+public func union(rhs: SQLStmt, lhs: SQLStmt) -> SQLStmt {
+    let union = SQLStmt()
+    union.append(enclosed([rhs]))
+    union.append("UNION", params: [])
+    union.append(enclosed([lhs]))
+    return union
+}
